92f324a71f0b8c8a857228cecffefd48ce4a3229
Ghifari <fauzighifari5@gmail.com>
2025-08-04 19:36:24 +0700

    turunkan versi react ke 18 dan next ke 14

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import HeroSection from "./components/fragments/HeroSection";
 import Services from "./components/fragments/Service";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
